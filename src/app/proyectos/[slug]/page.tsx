e25bcc9ee42d15a80115cb94193d434f0729c1d7
Jordan Talledo <programadortalledo@gmail.com>
2025-08-26 21:17:29 +0000

    sino se puede arreglar borra esa funcion

--- src/app/proyectos/[slug]/page.tsx
+++ src/app/proyectos/[slug]/page.tsx
@@ -1,45 +1,12 @@
 
+"use client";
+
 import { notFound } from 'next/navigation';
 import { projects } from '@/lib/data';
-import { Metadata, ResolvingMetadata } from 'next';
 import ProjectDetailClient from './project-detail-client';
 
 type Props = {
   params: { slug: string }
-}
-
-export async function generateMetadata(
-  { params }: Props,
-  parent: ResolvingMetadata
-): Promise<Metadata> {
-  const slug = params.slug;
-  const project = projects.find((p) => p.slug === slug);
-
-  if (!project) {
-    return {
-      title: 'Project Not Found',
-    }
-  }
-
-  const projectDescription = project.description_es;
-  
-  const previousImages = (await parent).openGraph?.images || []
-
-  return {
-    title: `${project.title} | Jordan Talledo Projects`,
-    description: projectDescription,
-    openGraph: {
-      title: `${project.title} | Jordan Talledo`,
-      description: projectDescription,
-      images: [project.imageUrl, ...previousImages],
-    },
-    twitter: {
-      card: 'summary_large_image',
-      title: `${project.title} | Jordan Talledo`,
-      description: projectDescription,
-      images: [project.imageUrl],
-    },
-  }
 }
 
 export default function ProjectDetailPage({ params }: Props) {
